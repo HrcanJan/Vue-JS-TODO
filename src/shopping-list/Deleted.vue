b2c785c3752779dc53b1de725bd9bce22d236629
Hrćan Jan <xhrcan@stuba.sk>
2023-09-23 12:24:38 +0200

    Feat: Implement functionality

--- src/shopping-list/Deleted.vue
+++ src/shopping-list/Deleted.vue
@@ -1,3 +1,16 @@
 <template>
-    <h1>Deleted</h1>
+    <h2>Zmazané položky</h2>
+	<div v-for="item in this.items" :key="`item-${item.id}`">
+		<div v-if="item.deleted">{{  item.text }}</div>
+	</div>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			items: JSON.parse(sessionStorage.getItem("items")) ? JSON.parse(sessionStorage.getItem("items")) : [],
+		}
+	}
+}
+</script>
--- src/shopping-list/List.vue
+++ src/shopping-list/List.vue
@@ -4,27 +4,18 @@
 	<button @click="addItem()">Pridať</button>
 	<hr>
 	<h2>Pridané položky</h2>
-	<ul>
-		<li v-for="item in this.items" :key="`item-${item.id}`">
-			<span @click="deleteItem(item)" style="margin-right: 15px" class="delete">X</span>
-			<span v-if="!item.deleted">{{  item.text }}</span>
-		</li>
-	</ul>
-	<hr>
-	<h2>Zmazané položky</h2>
-	<ul>
-		<li v-for="item in this.items" :key="`item-${item.id}`">
-			<s v-if="item.deleted">{{  item.text }}</s>
-		</li>
-	</ul>
+	<div v-for="item in this.items" :key="`item-${item.id}`">
+		<span v-if="!item.deleted" @click="deleteItem(item)" style="margin-right: 15px" class="delete">X</span>
+		<span v-if="!item.deleted">{{  item.text }}</span>
+	</div>
 </template>
 
 <script>
 export default {
 	data() {
 		return {
-			items: [],
-			input: ''
+			items: JSON.parse(sessionStorage.getItem("items")) ? JSON.parse(sessionStorage.getItem("items")) : [],
+			input: '',
 		}
 	},
 
@@ -35,11 +26,13 @@ export default {
 				text: this.input,
 				deleted: false
 			})
+			sessionStorage.setItem("items", JSON.stringify(this.items))
 			this.input = ''
 		},
 
 		deleteItem(item) {
 			item.deleted = true
+			sessionStorage.setItem("items", JSON.stringify(this.items))
 		}
 	},
 }
