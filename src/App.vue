4df830f8d0bff91ec746df1dd9efb4fc4f511d27
Hrćan Jan <xhrcan@stuba.sk>
2023-09-23 13:39:03 +0200

    style: stylize the navbar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <template>
-  <nav>
-    <router-link to="/">List</router-link> |
-    <router-link to="/deleted">Deleted</router-link>
-  </nav>
-  <router-view/>
+	<ul>
+		<li class="active" id="list" @click="activeTab('list')">
+			<router-link to="/">List</router-link>
+        </li>
+        <li id="deleted" @click="activeTab('deleted')">
+		    <router-link to="/deleted">Deleted</router-link>
+	    </li>
+    </ul>
+    <router-view/>
 </template>
 
 <script>
+export default {
+    methods: {
+        activeTab(id) {
+		    const tabs = document.getElementsByTagName('li')
+		    const activeTab = document.getElementById(id)
 
+	    for (let tab of tabs)
+		    tab.classList.remove('active')
+
+	    activeTab.classList.add('active')
+	    }
+    }
+}
 </script>
 
 <style scoped>
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	margin-top: 60px;
+    #app {
+	    font-family: Avenir, Helvetica, Arial, sans-serif;
+	    -webkit-font-smoothing: antialiased;
+	    -moz-osx-font-smoothing: grayscale;
+	    margin-top: 60px;
 }
 </style>
